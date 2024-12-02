2129c3923a06600e3eec0acb4255b494e60d3ce4
Joan Frago <jfragojimenez@gmail.com>
2024-12-02 15:49:33 +0100

    changes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+# RESOURCE MONITOR
 In this repository I'll make a systemd service to monitor system services
+
+## THINGS TO OUTPUT
+1. Network status (connected, not connected, what connected to)
+2. CPU status (freq, temp)
+3. Memory status (free, used)
+4. Disk status (free, used)
--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import os
 import sys
+import subprocess
 
+def checkNetwork():
+    # Use nmcli for Linux
+    iNet = []
+    try:
+        result = subprocess.run(
+            ["nmcli", "-t", "-f", "active,ssid", "dev", "wifi"],
+            capture_output=True, text=True
+        )
+        active_networks = [
+            line.split(":")[1]
+            for line in result.stdout.splitlines()
+            if line.startswith("yes")
+        ]
+        iNet.append(active_networks[0] if active_networks else None)
+
+        return iNet
+    except Exception as e:
+        print(f"Error checking Wi-Fi: {e}")
+        return None
+
+def printValues(aNet:list) -> bool:
+    try:
+        for value in aNet:
+            print(value)
+    
+        return True
+    except Exception as e:
+        return False
 
 if __name__ == "__main__":
-    
-    sys.exit()
+    try:
+        iNet = checkNetwork()
+        prnt = printValues(iNet)
+        if prnt:
+            sys.exit("\n")
+        else:
+            print("Could not print values")
+            sys.exit("\n")
+        sys.exit()
+    except KeyboardInterrupt:
+        sys.exit("\n")
